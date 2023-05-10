b4954c011d87aae65d31e5b2e14f12742f06aa41
EvgheniiMorogoi <jekamorogai@gmail.com>
2023-05-10 06:01:42 +0300

    LogIn And Register ViewModels

---
+++ FlowerShop/FlowerShop.Web/Models/Account/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FlowerShop.Web.Models.Account
+{
+     public class LoginViewModel
+     {
+          [Required(ErrorMessage = "You need to type the username")]
+          [Display(Name = "Username")]
+        public string Username { get; set; }
+
+          [Required(ErrorMessage ="You need to type the password")]
+          [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
---
+++ FlowerShop/FlowerShop.Web/Models/Account/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FlowerShop.Web.Models.Account
+{
+     public class RegisterViewModel
+     {
+          [Required]
+          [Display(Name = "Full name")]
+          public string FullName { get; set; }
+
+          [Required]
+          [Display(Name = "Username")]
+          public string Username { get; set; }
+
+          [Required]
+          [Display(Name = "Email")]
+          public string Email { get; set; }
+
+          [Required]
+          [Display(Name = "Password")]
+          public string Password { get; set; }
+
+          [Required]
+          [Display(Name = "Confirm password")]
+          public string ConfirmPassword { get; set; }
+     }
+}
