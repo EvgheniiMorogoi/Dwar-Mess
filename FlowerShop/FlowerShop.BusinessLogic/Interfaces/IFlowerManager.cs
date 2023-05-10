9222a767a050d581a328c32d2c07aff313528ace
AlexandruLasco <lasco00@mail.ru>
2023-05-10 03:27:50 +0300

    FlowerManager Interface, FlowerManager Class, FlowerDTO

---
+++ FlowerShop/FlowerShop.BusinessLogic/Interfaces/IFlowerManager.cs
@@ -0,0 +1,15 @@
+﻿using FlowerShop.BusinessLogic.Models;
+using FlowerShop.Data.DbModels;
+using System.Collections.Generic;
+
+namespace FlowerShop.BusinessLogic.Interfaces
+{
+    public interface IFlowerManager
+    {
+          List<FlowerDTO> GetAllFlowers();
+          FlowerDTO GetById(int id);
+          void AddNewFlower(FlowerDTO flower);
+          void DeleteById(FlowerDTO flower);
+          void Update(FlowerDTO flower);
+     }
+}
---
+++ FlowerShop/FlowerShop.BusinessLogic/Managers/FlowerManager.cs
@@ -0,0 +1,53 @@
+﻿using FlowerShop.BusinessLogic.Interfaces;
+using FlowerShop.BusinessLogic.Models;
+using FlowerShop.Data.DbModels;
+using FlowerShop.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.BusinessLogic.Managers
+{
+     public class FlowerManager : IFlowerManager
+     {
+          private readonly IFlowerRepository _flowerRepository;
+          public FlowerManager(IFlowerRepository flowerRepository)
+          {
+               _flowerRepository = flowerRepository;
+          }
+
+          public void AddNewFlower(FlowerDTO flower)
+          {
+               _flowerRepository.AddNewFlower(
+                    new FlowerDbModel()
+                    {
+                         Name = flower.Name,
+                         Description = flower.Description,
+                         Price = flower.Price
+                    });
+          }
+
+          public void DeleteById(FlowerDTO flower)
+          {
+               throw new NotImplementedException();
+               //_flowerRepository.DeleteById()
+          }
+
+          public List<FlowerDTO> GetAllFlowers()
+          {
+               return _flowerRepository.GetAllFlowers().Select(x => new FlowerDTO() { Name = x.Name, Description = x.Description, Price = x.Price }).ToList();
+          }
+
+          public FlowerDTO GetById(int id)
+          {
+               throw new NotImplementedException();
+          }
+
+          public void Update(FlowerDTO flower)
+          {
+               _flowerRepository.Update(new FlowerDbModel() { Name = flower.Name, Description = flower.Description, Price = flower.Price });
+          }
+     }
+}
