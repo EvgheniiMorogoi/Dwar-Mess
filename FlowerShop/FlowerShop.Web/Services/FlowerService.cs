5ef0bcc1411e521afcb87d18f43f993c8d575244
AlexandruLasco <lasco00@mail.ru>
2023-05-10 03:32:54 +0300

    Flower Services

---
+++ FlowerShop/FlowerShop.Web/Services/FlowerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FlowerShop.BusinessLogic.Interfaces;
+using FlowerShop.Web.Models.Flowers;
+using FlowerShop.Web.Services.Interfaces;
+
+namespace FlowerShop.Web.Services
+{
+     public class FlowerService : IFlowerService
+     {
+          private readonly IFlowerManager _flowerManager;
+          public FlowerService(IFlowerManager flowerManager)
+          {
+               _flowerManager = flowerManager;
+          }
+          public FlowerIndexViewModel GetAllFlowers()
+          {
+               return new FlowerIndexViewModel()
+               {
+                    Flowers = _flowerManager.GetAllFlowers().Select(x => new FlowerModel()
+                    {
+                         Name = x.Name,
+                         Description = x.Description,
+                         Price = x.Price
+                    }).ToList(),
+               };
+          }
+     }
+}
---
+++ FlowerShop/FlowerShop.Web/Services/Interfaces/IFlowerService.cs
@@ -0,0 +1,14 @@
+﻿using FlowerShop.Web.Models.Flowers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShop.Web.Services.Interfaces
+{
+     public interface IFlowerService
+     {
+          FlowerIndexViewModel GetAllFlowers();
+     }
+}
