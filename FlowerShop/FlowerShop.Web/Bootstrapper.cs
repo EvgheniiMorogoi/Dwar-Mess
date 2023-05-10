44155962779b3735e80cff7d8cefe8589a5dbf9c
AlexandruLasco <lasco00@mail.ru>
2023-05-10 03:31:59 +0300

    App Views product Index, Login Page, App LayOut

---
+++ FlowerShop/FlowerShop.Web/Bootstrapper.cs
@@ -0,0 +1,34 @@
+using System.Web.Mvc;
+using FlowerShop.BusinessLogic.Interfaces;
+using FlowerShop.BusinessLogic.Managers;
+using FlowerShop.Data;
+using FlowerShop.Data.Repositories;
+using FlowerShop.Data.Repositories.Interfaces;
+using FlowerShop.Web.Services;
+using FlowerShop.Web.Services.Interfaces;
+using Microsoft.Practices.Unity;
+using Unity.Mvc3;
+
+namespace FlowerShop.Web
+{
+     public static class Bootstrapper
+     {
+          public static void Initialise()
+          {
+               var container = BuildUnityContainer();
+
+               DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+          }
+
+          private static IUnityContainer BuildUnityContainer()
+          {
+               var container = new UnityContainer();
+
+               container.RegisterType<IFlowerService, FlowerService>();
+               container.RegisterType<IFlowerManager, FlowerManager>();
+               container.RegisterType<IFlowerRepository, FlowerRepository>();
+               container.RegisterType<IAppDataBaseContext, AppDataBaseContext>();
+               return container;
+          }
+     }
+}
